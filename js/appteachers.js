930287b71f0ed416cdbb8c27f0c3d7d6fcc29867
Samahara <le.chat.au.chapeau@hotmail.com>
2017-12-13 16:46:00 -0600

    actualización 6:54

--- js/appteachers.js
+++ js/appteachers.js
@@ -23,12 +23,12 @@ var select = function() {
   }
 }
 
+// Funciones para los botones del menú (No mover)
 var maincontainer = document.getElementById('main_container');
 var buttonCoders = document.getElementById('coders');
 var buttonOverview = document.getElementById('overview');
 var buttonTeachers = document.getElementById('teachers');
 
-// Funciones para los botones del menú
 var selectOption = function() {
   var option = event.target;
   console.log(option);
