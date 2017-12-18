a8ba971db0f98fa6586abb6d75614d45dfd2238d
Samahara <le.chat.au.chapeau@hotmail.com>
2017-12-18 11:50:53 -0600

    actualización domingo-lunes

--- js/app.js
+++ js/app.js
@@ -1,2 +1,25 @@
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
+
+// Variables reutilizables
+var maincontainer = document.getElementById('main_container');
+var buttonCoders = document.getElementById('coders');
+var buttonOverview = document.getElementById('overview');
+var buttonTeachers = document.getElementById('teachers');
+
+// Funciones para los botones del menú
+var selectOption = function() {
+  var option = event.target;
+  console.log(option);
+  if(option === buttonCoders){
+    maincontainer.style.backgroundColor = "pink";
+  }else if(option === buttonOverview){
+    maincontainer.style.backgroundColor = "green";
+  }else if(option === buttonTeachers){
+    maincontainer.style.backgroundColor = "blue";
+  }
+}
+
+buttonCoders.addEventListener('click', selectOption);
+buttonOverview.addEventListener('click', selectOption);
+buttonTeachers.addEventListener('click', selectOption);
