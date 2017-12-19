3b3c6efa81c91407fed4eda2601b5d82ea326d97
Ks-Mosqueda <ks.mosqueda@gmail.com>
2017-12-18 18:41:05 -0600

    subiendo link entre carpetas

--- js/appcodersdata.js
+++ js/appcodersdata.js
@@ -1,3 +1,24 @@
 console.log(data);
 
-var selectbutton = document.getElementById("btn")
+
+// Variables reutilizables y función para los botones del menú (No mover)
+var maincontainer = document.getElementById('main_container');
+var buttonCoders = document.getElementById('coders');
+var buttonOverview = document.getElementById('overview');
+var buttonTeachers = document.getElementById('teachers');
+
+var selectOption = function() {
+  var option = event.target;
+  console.log(option);
+  if(option === buttonCoders){
+    window.location="../views/coders.html";
+  }else if(option === buttonOverview){
+    window.location="../views/sede.html";
+  }else if(option === buttonTeachers){
+    window.location="../views/teachers.html";
+  }
+}
+
+buttonCoders.addEventListener('click', selectOption);
+buttonOverview.addEventListener('click', selectOption);
+buttonTeachers.addEventListener('click', selectOption);
